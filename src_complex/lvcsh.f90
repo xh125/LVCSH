827c269cfb4eaf14066c9a33b4950da932bc0b93
xh125 <xh125@mail.ustc.edu.cn>
2021-09-03 10:49:26 +0800

    change mdfiles

--- src_complex/lvcsh.f90
+++ src_complex/lvcsh.f90
@@ -16,11 +16,11 @@ program lvcsh
   !============================================================================!
   != materials interaction with environment by system-bath interactions       =!     
   !============================================================================!
-  != Last updata 2021-08.31 Version:0.6.1                                     =!   
+  != Last updata 2021-08.31 Version:0.6.4                                     =!   
   != Developed by XieHua at department of physic, USTC;xh125@mail.ustc.edu.cn =!
   !============================================================================!
   !! Author: HuaXie                                                           =!
-  !! Version: v0.6.1                                                          =!
+  !! Version: v0.6.4                                                          =!
   !! License: GNU                                                             =!
   !!===========================================================================!
   use mkl_service
@@ -415,7 +415,7 @@ program lvcsh
             call nonadiabatic_transition_scfssh(lfeedback,nefre,nefre_sh,nqtotf,nmodes,iesurface,E0_e,P0_e,d0_e,g_e,phP)              
           elseif(methodsh == "CC-FSSH") then
             call nonadiabatic_transition_ccfssh(lfeedback,nefre,nefre_sh,nqtotf,nmodes,iesurface,iesurface_j,&
-         &esurface_type,E0_e,P0_e,P_e,d0_e,S_bi_e,g_e,phP)
+                 &esurface_type,E0_e,P0_e,P_e,d0_e,S_bi_e,g_e,phP)
           endif
           
         endif
